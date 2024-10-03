c65367f4789e39f6f05976b0f74a3276ce546535
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2024-10-03 20:46:50 +0500

    JavaScript Assignment
    JavaScript Assignment

---
+++ app.js
@@ -0,0 +1,43 @@
+// Assignment 09:
+
+// Question 01:
+// Find Missing Number:
+
+// let numbers = [1,2,3,4,6,7,8,9];
+// let n = 10;
+// let totalSum = 0
+// let arrSum = 0
+// for(let i = 1; i <= n; i++){
+//     totalSum = +i    
+// }
+// for(let i = 0; i < numbers.length; i++){
+//  arrSum += numbers[i]
+// }
+
+// let missingNumber = totalSum - arrSum;
+
+// console.log("Missing number is:", missingNumber);
+
+
+// Question 02:
+// var numbers = [22,23,24,25,26,27,28,30]
+// var start = 22;
+// var end = 30
+// var totalSum = 0
+// var arrSum = 0
+
+// for(var i = start; i <= end; i++){
+//     totalSum += i
+// }
+// for(var i = 0; i < numbers.length; i++){
+//     arrSum += numbers[i]    
+// }
+// var missingNumber = totalSum - arrSum;
+
+// console.log("Missing number is:", missingNumber);
+
+
+
+
+
+
